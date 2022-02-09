55fed771288045f5c65a940377e157a6ecd8e4d3
kaito12c <kaito.cesc@gmail.com>
2022-02-10 02:22:35 +0900

    herokuの設定をしました。2

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,8 +1,9 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
 
 class CreateUsersTable extends Migration
 {
@@ -19,7 +20,7 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('birthday');
-            $table->string('email', 230) ->unique();
+            $table->string('email', 191) ->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
